d37c67e7c3d91a11ac1af7edfb8b4cc3ba821a20
strothj <jason@jasons.io>
2020-05-22 08:11:38 -0400

    refactor: rename entry point

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-async function main(): Promise<void> {
-  console.log("test");
-  throw new Error("test");
-}
-
-main().catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+async function main(): Promise<void> {}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
